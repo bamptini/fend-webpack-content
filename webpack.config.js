4ae6d5ea25d1517743e4d48fe284cfb10b1fe488
Bampton, James (jb704y) <jb704y@intl.att.com>
2021-05-14 07:11:52 +0100

    addd json -loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
 const path = require('path')
 const webpack = require('webpack')
+const json = require('')
 
 module.exports = {
     mode: 'development',
     entry: './src/client/index.js',
         module:{
-                    rules:[ {
+                    rules:[ 
+                        
+                        {
                         test: '/\.js$/',
                         exclude: /node_modules/,
                         loader: "babel-loader"
-                            }
+                        }
+
+                        {
+                        test2 :'/\.json$/',
+                        exclude: /node_modules/,
+                        loader: "json-loader"
+                        }
 
 
                     ]
